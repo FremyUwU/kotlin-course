43c84ea3791e5fb192d104f5a13446720d7996c8
FremyUwU <endergosha660@yandex.ru>
2024-09-13 19:00:23 +0500

    Initial commit

--- src/main/kotlin/org/george/kotlincourse/Main.kt
+++ src/main/kotlin/org/george/kotlincourse/Main.kt
@@ -1,4 +1,4 @@
-package com.george
+package com.george.org.george.kotlincourse
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
